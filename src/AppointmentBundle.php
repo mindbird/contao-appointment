522241c86614177f126744df94806d88c2d889d2
Florian Otto <florian.otto@mindbird.de>
2020-03-26 16:16:28 +0100

    Work work work work

---
+++ src/AppointmentBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Mindbird\Contao\Appointment;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppointmentBundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Mindbird\Contao\Appointment\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\SkeletonBundle\ContaoSkeletonBundle;
+use Mindbird\Contao\Appointment\AppointmentBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(AppointmentBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
